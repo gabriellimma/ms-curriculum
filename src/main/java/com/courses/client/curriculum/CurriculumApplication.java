a1e70170bf55a5a11d1272f216c074f51fec5028
biel42 <nickspoot@gmail.com>
2021-04-22 19:16:51 -0300

    project setup

---
+++ src/main/java/com/courses/client/curriculum/CurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.courses.client.curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courses/client/curriculum/CurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.client.curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
